d9915a0799853d5ce48bbb151d2314b4258978f4
Matias Asenjo Pino <matia.cba74@gmail.com>
2022-07-17 16:00:39 -0300

    daisy

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Robot from '../../public/img/robot2.png'
+const NavBar = () => {
+  return (
+    <>
+    <div class="not-prose grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 my-10 gap-6">
+   <a
+        class="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 transition-all duration-200 hover:shadow hover:-translate-y-1"
+        href="https://stackblitz.com/edit/daisyui-vite"
+        target="_blank"
+        rel="noopener"
+      >
+        <figure class="px-12 pt-6 pb-2 w-full aspect-[2/1] items-end">
+          <img class="w-full h-auto" src={Robot} alt="Vite" />
+        </figure>{" "}
+        <div class="card-body text-center">
+          <span class="text-xs">Vite</span>
+        </div>{" "}
+      </a>
+      <a
+        class="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 transition-all duration-200 hover:shadow hover:-translate-y-1"
+        href="https://stackblitz.com/edit/daisyui-vite"
+        target="_blank"
+        rel="noopener"
+      >
+        <figure class="px-12 pt-6 pb-2 w-full aspect-[2/1] items-end">
+          <img class="w-full h-auto" src={Robot} alt="Vite" />
+        </figure>{" "}
+        <div class="card-body text-center">
+          <span class="text-xs">Vite</span>
+        </div>{" "}
+      </a>
+      <a
+        class="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 transition-all duration-200 hover:shadow hover:-translate-y-1"
+        href="https://stackblitz.com/edit/daisyui-vite"
+        target="_blank"
+        rel="noopener"
+      >
+        <figure class="px-12 pt-6 pb-2 w-full aspect-[2/1] items-end">
+          <img class="w-full h-auto" src={Robot} alt="Vite" />
+        </figure>{" "}
+        <div class="card-body text-center">
+          <span class="text-xs">Vite</span>
+        </div>{" "}
+      </a>
+      <a
+        class="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 transition-all duration-200 hover:shadow hover:-translate-y-1"
+        href="https://stackblitz.com/edit/daisyui-vite"
+        target="_blank"
+        rel="noopener"
+      >
+        <figure class="px-12 pt-6 pb-2 w-full aspect-[2/1] items-end">
+          <img class="w-full h-auto" src={Robot} alt="Vite" />
+        </figure>{" "}
+        <div class="card-body text-center">
+          <span class="text-xs">Vite</span>
+        </div>{" "}
+      </a>
+      <a
+        class="card border-2 border-base-200 card-compact bg-white/5 hover:bg-gray-300/10 transition-all duration-200 hover:shadow hover:-translate-y-1"
+        href="https://stackblitz.com/edit/daisyui-vite"
+        target="_blank"
+        rel="noopener"
+      >
+        <figure class="px-12 pt-6 pb-2 w-full aspect-[2/1] items-end">
+          <img class="w-full h-auto" src={Robot} alt="Vite" />
+        </figure>{" "}
+        <div class="card-body text-center">
+          <span class="text-xs">Vite</span>
+        </div>{" "}
+      </a>
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default NavBar
