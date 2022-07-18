75bfb853b4fe68dc108cad889423c3e96b75da32
Matias Asenjo Pino <matia.cba74@gmail.com>
2022-07-17 23:18:19 -0300

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@
 import Card from "./components/Card"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-import NavBar from "./components/Navbar"
+import Navbar from "./components/Navbar"
 import Skills from "./components/Skills"
 
 function App() {
   return (
     <>
-    <NavBar></NavBar>
+    <Navbar></Navbar>
     <Header></Header>
     <Skills/>
     <Card></Card>
